30bd6aa9d1d511d7fcd43426653ce4dd05e2cd55
hongmingu <gmh397@nate.com>
2023-07-06 21:51:42 +0900

    change for mart

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : '연예인, 연예기획사, 뷰티 정보';
+    : '생활에 도움되는 팁 정보';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : '연예인, 연예기획사, 뷰티 정보 블로그';
+    : '여러가지 도움되는 정보를 기록합니다.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
