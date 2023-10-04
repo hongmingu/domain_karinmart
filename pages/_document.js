604acdcb7823efe666a9b0c553d2f537ba3a6ec3
hongmingu <gmh397@nate.com>
2023-10-04 15:11:26 +0900

    GTM

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,17 @@ class MyDocument extends Document {
       <Html lang="en" className="theme-compiled">
         <Head>
           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NZLD9MJ7');
+              `,
+            }}
+          />
+          <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8253099368332750"
             crossorigin="anonymous"
@@ -15,6 +26,14 @@ class MyDocument extends Document {
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NZLD9MJ7"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
