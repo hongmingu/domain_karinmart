7ee8990c1ac0f3e94a9888a9a82847e7f16a78e1
hongmingu <gmh397@nate.com>
2023-10-05 14:02:59 +0900

    gtm

--- pages/_document.js
+++ pages/_document.js
@@ -9,11 +9,11 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NZLD9MJ7');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5NP2XZ57');
               `,
             }}
           />
@@ -22,30 +22,16 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8253099368332750"
             crossOrigin="anonymous"
           ></script>
-
-
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-RLD76BBH04"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-RLD76BBH04');
-              `,
-            }}
-          />
-
         </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NZLD9MJ7"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5NP2XZ57"
               height="0"
               width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
+              style="display:none;visibility:hidden"
             ></iframe>
           </noscript>
           <Main />
